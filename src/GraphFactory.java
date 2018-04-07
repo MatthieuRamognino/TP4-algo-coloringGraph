c1f3bbec6c0ae54c287f3c710206eabdaf802770
MatthieuRamognino <matthieu.ramognino@club-internet.fr>
2018-04-07 17:25:23 +0200

    lecture txt

--- src/GraphFactory.java
+++ src/GraphFactory.java
@@ -1,30 +1,52 @@
 import java.io.IOException;
-import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class GraphFactory {
 	
 	public static  UndirectedAdjGraph<Integer> createDiGraphFromTextFile(String path) {
-		List<String> joueurs= new ArrayList<>();
+		List<Integer> joueurs= new ArrayList<>();
 		List<Integer> matchsDuJoueur= new ArrayList<>();
 		UndirectedAdjGraph<Integer> g = new UndirectedAdjGraph<>();
-		try(Scanner scan = new Scanner(FileSystems.getDefault().getPath(path))){
-			while(scan.hasNext()) {
-				for(int i=0;i<9;i++) {
-					if(scan.next()!="vs") {
-						String joueur = scan.next();
-						if(joueurs.contains(joueur)==false) {
-							joueurs.add(joueur);
+		
+			List<Integer> nombres = new ArrayList<>();
+			try {
+				for (String ligne : Files.readAllLines(Paths.get(path))) {
+					 for (String chaine : ligne.split(" ")){
+						 if(chaine.length()>2) {
+							 String test = chaine.replace("J-","");
+							 int test2= Integer.parseInt(test);
+							 System.out.println(test2);
+							 nombres.add(test2);
+						 }
+					 }
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			for(int i=0;i<nombres.size();i++) {
+				if(joueurs.contains(nombres.get(i))==false) {
+					joueurs.add(nombres.get(i));
+					matchsDuJoueur.clear();
+					for(int j=i;j<nombres.size();j++) {
+						if(nombres.get(j)==nombres.get(i)) {
+							matchsDuJoueur.add(j/10);
+						}
+					}
+					for(int k=0;k<matchsDuJoueur.size();k++) {
+						for(int l=k+1;l<matchsDuJoueur.size();l++) {
+							if(g.vertices().contains(matchsDuJoueur.get(k)) && 
+									g.inNeighbors(matchsDuJoueur.get(k)).contains(matchsDuJoueur.get(l))==false || 
+									g.vertices().contains(matchsDuJoueur.get(k))==false) {
+								g.addEdge(matchsDuJoueur.get(k), matchsDuJoueur.get(l));
+							}
 						}
 					}
 				}
-				g.addEdge(u, v);
-			}	
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			}
 		return g;
 	}
 }
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main extends GraphFactory{
+
+	public static void main(String[] args) {
+		UndirectedAdjGraph<Integer> g=createDiGraphFromTextFile("Graph/Tournament-data.txt");
+
+	}
+
+}
